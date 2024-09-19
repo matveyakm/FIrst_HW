5b93ec18377ea46b9880ddcebd6496ac8d01faf5
matveyakm <102146670+matveyakm@users.noreply.github.com>
2024-09-19 17:27:10 +0300

    Update 0809245.c

--- 0809245.c
+++ 0809245.c
@@ -1,30 +1,34 @@
 #include <stdio.h>
 #include <string.h>
-//Напишите алгоритм проверки баланса скобок в исходной строке (т.е. число открывающих скобок равно числу закрывающих и выполняется правило вложенности скобок). Скобки бывают только одного типа.
-int bracketCheck(char inp[])
+#include <stdlib.h>
+#include <string.h>
+
+// Напишите алгоритм проверки баланса скобок в исходной строке (т.е. число открывающих скобок равно числу закрывающих и выполняется правило вложенности скобок). Скобки бывают только одного типа.
+
+// UPD: Из условия задачи, скобки одного типа, но не сказано какого. Т.е. либо только круглый, либо квадратный и тд. В такой трактовке условия, код работает корректно
+
+int bracketCheck(char *localString)
 {
     int bracketsBalance = 0;
-    for(int i = 0; i < 128; i++)
-    {
-        char currentChar = inp[i];
-        printf("%c %d\n", currentChar, currentChar);
-        if (currentChar == 40 | currentChar == 123 | currentChar == 91)
-        {
+    for (int i = 0; i < strlen(localString); i++) {
+        char currentChar = localString[i];
+        if (currentChar == '(' || currentChar == '{' || currentChar == '[') {
             bracketsBalance++;
         }
-        if (currentChar == 41 | currentChar == 125 | currentChar == 93)
-        {
+        if (currentChar == ')' || currentChar == '}' || currentChar == ']') {
             bracketsBalance--;
         }
-        if (bracketsBalance < 0 | currentChar == 0) {break;}
+        if (bracketsBalance < 0 || currentChar == 0) break; // currentChar уже заканчивал перебор, если строка окончена
     }
-    return (bracketsBalance == 0);
+    return bracketsBalance == 0;
 }
 
 int main()
 {
-    char inp[128];
-    fgets(inp, sizeof(inp), stdin);
-    printf("%d",bracketCheck(inp));
+    size_t size = 16;
+    char *inputString = (char *)malloc(size * sizeof(char)); 
+    getline(&inputString, &size, stdin);
+
+    printf("%d",bracketCheck(inputString));
     return 0;
 }
